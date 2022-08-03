1f00251a5e2f8f8de45d8f218eccfbf6c8eb64d6
Michael Brauweiler <michael.brauweiler@posteo.de>
2022-08-03 18:18:16 +0200

    Improve admin panel

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,10 @@ from django.views.generic import TemplateView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.authtoken.views import obtain_auth_token
 
+admin.site.site_header = "Graph Watch"
+admin.site.index_title = "Control Panel"
+admin.site.site_title = "Graph Watch"
+
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path(
