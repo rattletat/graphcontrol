14fab92f5a1f4ddf375d82afe624064af47e359a
Michael Brauweiler <michael.brauweiler@posteo.de>
2022-09-22 03:49:54 +0200

    Modify periodic tasks

--- graphwatch/twitter/tasks/periodic.py
+++ graphwatch/twitter/tasks/periodic.py
@@ -64,9 +64,14 @@ def fetch_random_user_following(limit=1000):
 
 @celery_app.task(name="Fetch Oldest User Tweets", max_retries=1)
 def fetch_oldest_user_tweets(limit=1000):
-    """Fetches the thousand most recent tweets of a random Twitter user.
+    """Fetches the thousand most recent tweets of the
+    oldest real twitter user with zero tweets.
     Returns list of fetched Tweet ids."""
-    oldest_account = Account.objects.order_by("created").filter(tweets=None).first()
+    oldest_account = (
+        Account.objects.order_by("created")
+        .filter(tweets=None, handle__isnull=True)
+        .first()
+    )
     try:
         update.update_tweets.delay(oldest_account.twitter_id, limit=limit)
     except tweepy_errors.Unauthorized:
@@ -75,10 +80,14 @@ def fetch_oldest_user_tweets(limit=1000):
 
 @celery_app.task(name="Fetch Oldest User Following", max_retries=1)
 def fetch_oldest_user_following(limit=1000):
-    """Fetches the thousand most recent tweets of the
-    oldest twitter user with zero follows.
+    """Fetches the thousand most recent follows of the
+    oldest real twitter user with zero follows.
     Returns list of fetched Tweet ids."""
-    oldest_account = Account.objects.order_by("created").filter(following=None).first()
+    oldest_account = (
+        Account.objects.order_by("created")
+        .filter(following=None, handle__isnull=True)
+        .first()
+    )
     try:
         update.update_following.delay(oldest_account.twitter_id, limit=limit)
     except tweepy_errors.Unauthorized:
@@ -87,10 +96,14 @@ def fetch_oldest_user_following(limit=1000):
 
 @celery_app.task(name="Fetch Oldest User Followers", max_retries=1)
 def fetch_oldest_user_followers(limit=1000):
-    """Fetches the thousand most recent tweets of the
-    oldest twitter user with zero followers.
+    """Fetches the thousand most recent followers of the
+    oldest real twitter user with zero followers.
     Returns list of fetched Tweet ids."""
-    oldest_account = Account.objects.order_by("created").filter(following=None).first()
+    oldest_account = (
+        Account.objects.order_by("created")
+        .filter(following=None, handle__isnull=True)
+        .first()
+    )
     try:
         update.update_followers.delay(oldest_account.twitter_id, limit=limit)
     except tweepy_errors.Unauthorized:
