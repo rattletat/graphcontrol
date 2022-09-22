58e4791905e73eb1ebcbbb744426f5cd827eb3d2
Michael Brauweiler <michael.brauweiler@posteo.de>
2022-09-22 03:35:06 +0200

    Add new periodic method

--- graphwatch/twitter/tasks/periodic.py
+++ graphwatch/twitter/tasks/periodic.py
@@ -30,33 +30,57 @@ from . import update
 
 
 @celery_app.task(name="Fetch Random User Tweets", max_retries=1)
-def fetch_random_user_tweets(count=100):
-    """Fetches the hundred most recent tweets of a random Twitter user.
+def fetch_random_user_tweets(limit=1000):
+    """Fetches the thousand most recent tweets of a random Twitter user.
     Returns list of fetched Tweet ids."""
     random_account = Account.objects.order_by("?")[0]
     try:
-        update.update_tweets.delay(random_account.twitter_id, count=count)
+        update.update_tweets.delay(random_account.twitter_id, limit=limit)
     except tweepy_errors.Unauthorized:
         pass
 
 
 @celery_app.task(name="Fetch Random User Followers", max_retries=1)
-def fetch_random_user_followers():
-    """Fetches the hundred most recent tweets of a random Twitter user.
+def fetch_random_user_followers(limit=1000):
+    """Fetches the thousand most recent tweets of a random Twitter user.
     Returns list of fetched Tweet ids."""
     random_account = Account.objects.order_by("?")[0]
     try:
-        update.update_followers.delay(random_account.twitter_id)
+        update.update_followers.delay(random_account.twitter_id, limit=limit)
     except tweepy_errors.Unauthorized:
         pass
 
 
 @celery_app.task(name="Fetch Random User Following", max_retries=1)
-def fetch_random_user_following():
+def fetch_random_user_following(limit=1000):
     """Fetches the hundred most recent tweets of a random Twitter user.
     Returns list of fetched Tweet ids."""
     random_account = Account.objects.order_by("?")[0]
     try:
-        update.update_following.delay(random_account.twitter_id)
+        update.update_following.delay(random_account.twitter_id, limit=limit)
+    except tweepy_errors.Unauthorized:
+        pass
+
+
+@celery_app.task(name="Fetch Oldest User Following", max_retries=1)
+def fetch_oldest_user_following(limit=1000):
+    """Fetches the thousand most recent tweets of the
+    oldest twitter user with zero follows.
+    Returns list of fetched Tweet ids."""
+    oldest_account = Account.objects.order_by("created").filter(following=None).first()
+    try:
+        update.update_following.delay(oldest_account.twitter_id, limit=limit)
+    except tweepy_errors.Unauthorized:
+        pass
+
+
+@celery_app.task(name="Fetch Oldest User Followers", max_retries=1)
+def fetch_oldest_user_followers(limit=1000):
+    """Fetches the thousand most recent tweets of the
+    oldest twitter user with zero followers.
+    Returns list of fetched Tweet ids."""
+    oldest_account = Account.objects.order_by("created").filter(following=None).first()
+    try:
+        update.update_followers.delay(oldest_account.twitter_id, limit=limit)
     except tweepy_errors.Unauthorized:
         pass
